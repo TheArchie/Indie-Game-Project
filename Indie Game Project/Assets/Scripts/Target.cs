faab1b2c11c6fa1f493f3536a42acf70f5b7f0fe
TheArchie <44277291+TheArchie@users.noreply.github.com>
2019-01-23 21:24:35 +0000

    Muzzle Flash

---
+++ Indie Game Project/Assets/Scripts/Target.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public float health = 100f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Damage(float amountofDamage)
+    {
+        health -= amountofDamage;
+        if (health <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Indie Game Project/Assets/Scripts/Weapon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public float weaponDamage = 30f; 
+
+    public Camera cam;
+
+    public ParticleSystem muzzleFlash;
+
+     void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }		
+	}
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hitInfo;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo));
+        {
+            Debug.Log(hitInfo.transform.name);
+
+            Target target = hitInfo.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.Damage(weaponDamage);
+            }
+        }
+    }
+}
