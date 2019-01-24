6342edc8ac2c8ed7b556fae5caf50e44e195bf36
TheArchie <44277291+TheArchie@users.noreply.github.com>
2019-01-24 18:41:21 +0000

    lots and lots

---
+++ Indie Game Project/Assets/Scripts/SwitchingWeapons.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchingWeapons : MonoBehaviour {
+
+    public int weaponChoice = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SelectWeapon();
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        int previousweaponChoice = weaponChoice;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (weaponChoice >= transform.childCount - 1)
+                weaponChoice = 0;
+            else
+                weaponChoice++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (weaponChoice <= 0)
+                weaponChoice = transform.childCount - 1;
+            else
+                weaponChoice--;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1) && transform.childCount >=1)
+        {
+            weaponChoice = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            weaponChoice = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            weaponChoice = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        {
+            weaponChoice = 3;
+        }
+
+        if (previousweaponChoice != weaponChoice)
+        {
+            SelectWeapon();
+        }
+    }
+
+    void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == weaponChoice)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
+        }
+    }
+}
--- Indie Game Project/Assets/Scripts/Weapon.cs
+++ Indie Game Project/Assets/Scripts/Weapon.cs
@@ -4,39 +4,102 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
 
-    public float weaponDamage = 30f; 
+    public float weaponDamage = 30f; //Amount of damage the weapon does
+    public int maxAmmo = 60;
+    public int magazineAmmo = 20;
+   [SerializeField]
+    private int currentAmmo;
+    public float fireRate = 10f;
+    public float weaponSpread = 1f;
+    public float weaponRecoil = 1f; 
+    public float reloadTime = 1.5f;
+    private bool isReloading = false;
+
+    private float fireTime = 0f;
 
     public Camera cam;
 
+    public Animator animator;
+
     public ParticleSystem muzzleFlash;
+
+    [SerializeField]
+    private GameObject bulletDecal;
 
      void Start()
     {
+        currentAmmo = magazineAmmo;
       
     }
 
-    // Update is called once per frame
-    void Update ()
+     void OnEnable()
     {
-        if(Input.GetButtonDown("Fire1"))
+        isReloading = false;
+        animator.SetBool("isReloading", false);       
+    }
+
+
+    void Update()
+    {
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
         {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= fireTime) 
+        {
+            fireTime = Time.time + 1f / fireRate;
             Shoot();
-        }		
+            Debug.Log("Gun Fired");
+        }        
 	}
 
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Relaod");
+        animator.SetBool("isReloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+        animator.SetBool("isReloading", false);
+        yield return new WaitForSeconds(.25f);
+
+        int bulletstoload = magazineAmmo - currentAmmo;
+        int bulletoremove = (maxAmmo >= bulletstoload) ? bulletstoload : maxAmmo;
+        maxAmmo -= bulletoremove;
+        currentAmmo += bulletoremove;
+
+        isReloading = false;
+    }
+
+    //Shoot function that plays a muzzle flash 
     void Shoot()
     {
         muzzleFlash.Play();
+        currentAmmo--;
         RaycastHit hitInfo;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo));
         {
             Debug.Log(hitInfo.transform.name);
+            Debug.Log(currentAmmo);
 
             Target target = hitInfo.transform.GetComponent<Target>();
             if(target != null)
             {
                 target.Damage(weaponDamage);
             }
+            SpawnDecal(hitInfo);
         }
+    }
+
+    void SpawnDecal(RaycastHit hitInfo)
+    {
+        var decal = Instantiate(bulletDecal);
+        decal.transform.position = hitInfo.point;
+        decal.transform.forward = hitInfo.normal * -1f;
     }
 }
